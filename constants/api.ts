4a40ac287db5f3820cf5c7bde66f1e14c6ba418f
naveen2003git <naveenkumarthangaraj007@gmail.com>
2026-01-09 11:49:32 +0530

    Update api.ts

--- constants/api.ts
+++ constants/api.ts
@@ -3,113 +3,113 @@ export const API_ENDPOINTS = {
   CompanyUrl: "https://hr.trickyhr.com",
 
   // Authentication
-  LOGIN: "/WebApi/Login",
+  LOGIN: "/WebApi2/Login",
 
-  REFRESH_LOGIN: "/WebApi/RefreshLogin",
+  REFRESH_LOGIN: "/WebApi2/RefreshLogin",
 
   // Attendance
-  INSERT_MOBILE_ATTENDANCE: "/WebApi/InsertMobileAtten",
+  INSERT_MOBILE_ATTENDANCE: "/WebApi2/InsertMobileAtten",
 
-  ATTENDANCE_LIST: "/WebApi/GetEmployee_AttendanceList",
+  ATTENDANCE_LIST: "/WebApi2/GetEmployee_AttendanceList",
 
-  ATTENDANCE_REPORT: "/WebApi/GetMobileAttenRpt",
+  ATTENDANCE_REPORT: "/WebApi2/GetMobileAttenRpt",
 
   // Projects
-  GET_PROJECT_LIST: "/WebApi/GetProjectList",
+  GET_PROJECT_LIST: "/WebApi2/GetProjectList",
 
   // General
-  SERVER_TIME: "/WebApi/GetServerTime",
+  SERVER_TIME: "/WebApi2/GetServerTime",
 
   // Holidays
-  HOLIDAY: "/WebApi/GetHolidayList",
+  HOLIDAY: "/WebApi2/GetHolidayList",
 
   // Celebrations
-  CELEBRATION: "/WebApi/GetTodayCelebration",
+  CELEBRATION: "/WebApi2/GetTodayCelebration",
 
   // Leave Management
-  GET_LEAVE_DETAILS: "/WebApi/GetEmpLeaveApplyDtlById",
+  GET_LEAVE_DETAILS: "/WebApi2/GetEmpLeaveApplyDtlById",
 
-  GET_LEAVE_BALANCE_DETAILS: "/WebApi/GetLeaveBalanceDtlById",
+  GET_LEAVE_BALANCE_DETAILS: "/WebApi2/GetLeaveBalanceDtlById",
 
-  GET_LEAVE_MANAGE: "/WebApi/GetEmpLeaveManageById",
+  GET_LEAVE_MANAGE: "/WebApi2/GetEmpLeaveManageById",
 
-  GET_LEAVE_BALANCE: "/WebApi/GetEmpLeaveBalanceByEmpId",
+  GET_LEAVE_BALANCE: "/WebApi2/GetEmpLeaveBalanceByEmpId",
 
-  GET_SURRENDER_BALANCE: "/WebApi/GetEmpLVSurrenderBalance",
+  GET_SURRENDER_BALANCE: "/WebApi2/GetEmpLVSurrenderBalance",
 
-  GET_SURRENDER_DETAILS: "/WebApi/GetEmpLvSurrenderById",
+  GET_SURRENDER_DETAILS: "/WebApi2/GetEmpLvSurrenderById",
 
-  GET_DELETE_APPLY: "/WebApi/GetDeletApply",
+  GET_DELETE_APPLY: "/WebApi2/GetDeletApply",
 
   // Leave Operations
-  APPLY_LEAVE: "/WebApi/UpdateEmpLeaveApply",
+  APPLY_LEAVE: "/WebApi2/UpdateEmpLeaveApply",
 
-  SUBMIT_SURRENDER: "/WebApi/UpdateEmpSurrender",
+  SUBMIT_SURRENDER: "/WebApi2/UpdateEmpSurrender",
 
 
   // Medical Documents
-  UPLOAD_MEDICAL_DOC: "/WebApi/UploadMedicalDoc",
+  UPLOAD_MEDICAL_DOC: "/WebApi2/UploadMedicalDoc",
 
   // Manager Approvals
-  GET_PENDING_LEAVES: "/WebApi/GetSup_LeaveManageById",
+  GET_PENDING_LEAVES: "/WebApi2/GetSup_LeaveManageById",
 
-  GET_PENDING_SURRENDERS: "/WebApi/GetSup_LVSurrenderById",
+  GET_PENDING_SURRENDERS: "/WebApi2/GetSup_LVSurrenderById",
 
-  SAVE_APPROVAL: "/WebApi/SaveApproval_Supervisor",
+  SAVE_APPROVAL: "/WebApi2/SaveApproval_Supervisor",
 
   // Employee Info
-  GET_PROFILE: "/WebApi/GetEmpProfile",
+  GET_PROFILE: "/WebApi2/GetEmpProfile",
 
-  GET_PENDING_LIST: "/WebApi/GetPendingApprove_YourList",
+  GET_PENDING_LIST: "/WebApi2/GetPendingApprove_YourList",
 
-  GET_EMP_REQUEST_STATUS: "/WebApi/GetEmpRequestStatus",
+  GET_EMP_REQUEST_STATUS: "/WebApi2/GetEmpRequestStatus",
 
   // Time Management
-  GET_TIME_MANAGE_LIST: "/WebApi/GetEmpTimeManage",
+  GET_TIME_MANAGE_LIST: "/WebApi2/GetEmpTimeManage",
 
-  UPDATE_TIME: "/WebApi/UpdateEmpTimeManage",
+  UPDATE_TIME: "/WebApi2/UpdateEmpTimeManage",
 
-  DOWNLOAD_REPORT: "/WebApi/PrintMonthlyReport",
+  DOWNLOAD_REPORT: "/WebApi2/PrintMonthlyReport",
 
   // Uploaded Documents
-  GET_UPLOADED_DOCUMENTS: "/WebApi/GetEmpDocumentShow",
+  GET_UPLOADED_DOCUMENTS: "/WebApi2/GetEmpDocumentShow",
 
-  UPLOAD_DOCUMENT: "/WebApi/SaveEmpPortalDocument",
+  UPLOAD_DOCUMENT: "/WebApi2/SaveEmpPortalDocument",
 
   // Office Documents
-  GET_OFFICE_DOCUMENTS: "/WebApi/GetEmpDocumentsGridList",
+  GET_OFFICE_DOCUMENTS: "/WebApi2/GetEmpDocumentsGridList",
 
   // Payslip
-  GET_PAYSLIP_LIST: "/WebApi/GetEmpPaySalList",
+  GET_PAYSLIP_LIST: "/WebApi2/GetEmpPaySalList",
 
-  DOWNLOAD_PAYSLIP: "/WebApi/DownloadEmpPaySlip",
+  DOWNLOAD_PAYSLIP: "/WebApi2/DownloadEmpPaySlip",
 
   // Calendar
-  GET_CALENDAR_EVENTS: "/WebApi/GetLeaveCalender",
+  GET_CALENDAR_EVENTS: "/WebApi2/GetLeaveCalender",
 
-  GET_CALENDAR_DETAILS: "/WebApi/GetLeaveCalenderDtl",
+  GET_CALENDAR_DETAILS: "/WebApi2/GetLeaveCalenderDtl",
 
   // Employee List
-  GET_EMPLOYEE_LIST: "/WebApi/GetSup_EmployeeList",
+  GET_EMPLOYEE_LIST: "/WebApi2/GetSup_EmployeeList",
 
   // Attendance Report
-  GET_MOBILE_ATTENDANCE_REPORT: "/WebApi/GetMobileAttenRpt",
+  GET_MOBILE_ATTENDANCE_REPORT: "/WebApi2/GetMobileAttenRpt",
 
   // Service Report
-  GET_CLIENT_LIST: "/WebApi/GetService_ClientList",
+  GET_CLIENT_LIST: "/WebApi2/GetService_ClientList",
 
-  SUBMIT_SERVICE_REPORT: "/WebApi/Update_StatusRpt",
+  SUBMIT_SERVICE_REPORT: "/WebApi2/Update_StatusRpt",
 
   // Pending Approvals
-  GET_YOUR_PENDING_APPROVALS: "/WebApi/GetPendingApprove_YourList",
+  GET_YOUR_PENDING_APPROVALS: "/WebApi2/GetPendingApprove_YourList",
 
-  GET_OTHER_PENDING_APPROVALS: "/WebApi/GetPendingApprove_OtherList",
+  GET_OTHER_PENDING_APPROVALS: "/WebApi2/GetPendingApprove_OtherList",
 
-  UPDATE_PENDING_APPROVAL: "/WebApi/UpdatePendingApproval",
+  UPDATE_PENDING_APPROVAL: "/WebApi2/UpdatePendingApproval",
 
-  PEND_TIMEMNG_URL: "/WebApi/GetSup_TimeManageById",
-  PEND_CLAIM_URL: "/WebApi/GetSup_ClaimById",
+  PEND_TIMEMNG_URL: "/WebApi2/GetSup_TimeManageById",
+  PEND_CLAIM_URL: "/WebApi2/GetSup_ClaimById",
 
   // Approval Details
-  SUP_DETAPPROVE_URL: "/WebApi/GetApprovaledDetails",
+  SUP_DETAPPROVE_URL: "/WebApi2/GetApprovaledDetails",
 };
