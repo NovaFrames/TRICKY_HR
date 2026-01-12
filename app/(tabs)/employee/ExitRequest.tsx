3eb2a6f0ef42108a357335a4991f0e5f60fa1cd9
naveen2003git <naveenkumarthangaraj007@gmail.com>
2026-01-12 17:19:52 +0530

    Update ExitRequest.tsx

--- app/(tabs)/employee/ExitRequest.tsx
+++ app/(tabs)/employee/ExitRequest.tsx
@@ -298,6 +298,7 @@ export default function ExitRequestScreen() {
                                 mode="date"
                                 display="default"
                                 onChange={handleDateChange}
+                                minimumDate={new Date()}
                             />
                         )}
                     </View>
