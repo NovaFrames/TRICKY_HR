d3274d8521b3b43cdd01f2b276ba8965c4a8c5e1
Stalin <stalinchidhamparam@gmail.com>
2026-05-28 10:57:13 +0530

    updated version

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -403,7 +403,7 @@ export default function SettingsScreen() {
 
         <View style={styles.footer}>
                     <Text style={[styles.footerText, { color: theme.textLight }]}>
-                        Version 3.5.6 • © 2026
+                        Version 3.5.7 • © 2026
                     </Text>
                 </View>
       </ScrollView>
